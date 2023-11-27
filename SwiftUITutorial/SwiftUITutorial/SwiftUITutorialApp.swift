7861e07e2bab225c10214f3266c1ecaf545f74c7
Atsumi-Gakuto <k20006kk@aitech.ac.jp>
2023-11-27 14:39:37 +0900

    レッスン2-3

--- SwiftUITutorial/SwiftUITutorial/SwiftUITutorialApp.swift
+++ SwiftUITutorial/SwiftUITutorial/SwiftUITutorialApp.swift
@@ -11,7 +11,10 @@ import SwiftUI
 struct SwiftUITutorialApp: App {
     var body: some Scene {
         WindowGroup {
-            LandmarkRow(landmark: landmarks[0])
+            Group {
+                LandmarkRow(landmark: landmarks[0])
+                LandmarkRow(landmark: landmarks[1])
+            }
         }
     }
 }
