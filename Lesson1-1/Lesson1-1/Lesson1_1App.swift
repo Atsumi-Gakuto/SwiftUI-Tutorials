7907c4a46ea4da50a0d7e833939a8e090a398b4b
Atsumi-Gakuto <k20006kk@aitech.ac.jp>
2023-11-27 12:32:19 +0900

    レッスン1-1

---
+++ Lesson1-1/Lesson1-1/Lesson1_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson1_1App.swift
+//  Lesson1-1
+//
+//  Created by 渥美岳大 on 2023/11/27.
+//
+
+import SwiftUI
+
+@main
+struct Lesson1_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
