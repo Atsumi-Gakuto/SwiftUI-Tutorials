7fba36f529bcdea000bef5daa17e74bf861c5413
Atsumi-Gakuto <k20006kk@aitech.ac.jp>
2023-11-27 12:37:00 +0900

    レッスン1-2

---
+++ Lesson1-2/Lesson1-2/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  Lesson1-2
+//
+//  Created by 渥美岳大 on 2023/11/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Turtle Rock")
+            .font(.title)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lesson1-2/Lesson1-2/Lesson1_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson1_2App.swift
+//  Lesson1-2
+//
+//  Created by 渥美岳大 on 2023/11/27.
+//
+
+import SwiftUI
+
+@main
+struct Lesson1_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
