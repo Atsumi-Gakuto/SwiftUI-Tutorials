d4303d9d05c032c7a14ab58055743f9be5304962
Atsumi-Gakuto <k20006kk@aitech.ac.jp>
2023-11-27 12:40:19 +0900

    レッスンデータを移動

---
+++ lessons/Lesson1-3/Lesson1-3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lesson1-3
+//
+//  Created by 渥美岳大 on 2023/11/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ lessons/Lesson1-3/Lesson1-3/Lesson1_3App.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson1_3App.swift
+//  Lesson1-3
+//
+//  Created by 渥美岳大 on 2023/11/27.
+//
+
+import SwiftUI
+
+@main
+struct Lesson1_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
